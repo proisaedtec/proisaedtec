de4cf961a770fe1b1750132840c1a94d3997d25c
proisaedtec <139812639+proisaedtec@users.noreply.github.com>
2023-07-17 16:52:12 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(1000)
+    basic.showString("Ola!")
+})
